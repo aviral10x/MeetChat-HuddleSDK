f7f47735118c8285be781f2f279e99ee7adf9fd7
AVIRAL GUPTA <guptaaviral59@gmail.com>
2023-02-09 22:08:29 +0800

    test

--- src/components/meet.jsx
+++ src/components/meet.jsx
@@ -17,7 +17,7 @@ text-transparent xl:text-[5rem] md:text-6xl font-extrabold font-polySans md:max-
                     <p className="mb-3 font-normal text-center text-gray-300 dark:text-gray-400">Let's Start To Connect With People</p>
                     <a href="/match" className="inline-flex justify-center align-middle items-center p-5 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Jump Singapore Exclusive Chat Room
-                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        {/* <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
                     </a>
                 </div>
 
--- src/home.jsx
+++ src/home.jsx
@@ -3,31 +3,16 @@
 // import Image from 'next/image'
 import React from 'react'
 import Header from './components/header'
-import Hero from './components/hero'
+import Matching from './components/matching'
+// import Hero from './components/hero'
 
 const Home = () => {
     return (
-        <div className=" bg-black" >
-            <Header />
-            <Hero />
-            <div className="w-full items-center justify-center bg-black text-white">
-                <title>Meet3Club</title>
-                <link rel="icon" href="/favicon.ico" />
-            </div>
-
-            <footer className="flex h-25 p-5 w-full items-center justify-center bg-black text-white">
-                <a
-                    className="flex items-center justify-center gap-2"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    
-                >
-                    Powered by <b>Meet3Club</b>
-
-                </a>
-            </footer>
-        </div>
+        <div className='h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-indigo-900 via-gray-900 to-black'>
+        {/* <Header /> */}
+        <Matching />
+      
+    </div>
     )
 }
 
